3b8f074c5beac5eabeef63df6ad4bca1eea7ddfe
Des Moral <desnl13@yahoo.com>
2025-05-31 19:22:41 +0800

    Create README_script_create_proc_recalls_search_component_by_keyword_and_year.sql

---
+++ SQL_Case_Studies/recalls/README_script_create_proc_recalls_search_component_by_keyword_and_year.sql
@@ -0,0 +1,18 @@
+## stored procedure created using the UI
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `search_component_keyword`(
+IN pattern VARCHAR(100),
+`year` INT
+)
+BEGIN
+
+  SELECT 
+	YEAR(`date`) AS `year`
+    , manufacturer
+    , `component`
+    , recall_type 
+    , potentially_affected
+  FROM recalls_data_cleaned d
+  WHERE `component` REGEXP pattern AND `year` = YEAR(`date`);
+
+END
